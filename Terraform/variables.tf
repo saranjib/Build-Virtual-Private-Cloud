4af175fb940ba9fb9349a13b7a51407f25b5132e
saranjib <102132535+saranjib@users.noreply.github.com>
2026-08-11 04:30:16 +0530

    Add new variables for project configuration

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,3 +1,9 @@
+variable "project_name" {
+  description = "Project name"
+  type        = string
+  default     = "azure-vnet-project"
+}
+
 variable "resource_group_name" {
   description = "Azure resource group name"
   type        = string
@@ -8,4 +14,16 @@ variable "location" {
   description = "Azure region"
   type        = string
   default     = "East US"
+}
+
+variable "admin_username" {
+  description = "Linux VM administrator username"
+  type        = string
+  default     = "azureadmin"
+}
+
+variable "admin_password" {
+  description = "Linux VM administrator password"
+  type        = string
+  sensitive   = true
 }
