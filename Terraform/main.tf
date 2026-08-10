0c2b43fd56034f916cc5d7ef9fed79b2d1c2acd3
saranjib <102132535+saranjib@users.noreply.github.com>
2026-08-11 04:28:46 +0530

    Add Log Analytics workspace resource
    Added a Log Analytics workspace resource to the Terraform configuration.

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -4,3 +4,14 @@ resource "azurerm_resource_group" "rg-azure-vnet-project" {
   name     = var.resource_group_name
   location = var.location
 }
+
+resource "azurerm_log_analytics_workspace" "azure-vnet-project-law" {
+  name                = "${var.project_name}-law"
+  location            = azurerm_resource_group.rg-azure-vnet-project.location
+  resource_group_name = azurerm_resource_group.rg-azure-vnet-project.name
+
+  sku               = "PerGB2018"
+  retention_in_days = 30
+
+ 
+}
